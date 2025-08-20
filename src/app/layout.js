e04167329ade23ac1184543855d5cec05e89b74c
chamal <cchamal4@gmail.com>
2025-08-20 23:56:39 +0400

    Add real estate project files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playfair_Display, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-Poppins",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const playfair_display = Playfair_Display({
+  variable: "--font-Playfair_Display",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${playfair_display.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-heading text-4xl">Luxury Apartment</h1>
+      <p className="font-body text-base">Beautiful 3-bedroom apartment...</p>
+
+    </>
+  );
+}
